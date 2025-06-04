4baf34d7efe9c3f5a04cc84fa9410a75020aab84
Tristan Dunn <hello@tristandunn.com>
2025-06-03 20:21:15 -0400

    Add support for custom logger via configuration

---
+++ spec/ruby_llm_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RubyLLM do
+  describe '.logger' do
+    let(:logger) { instance_double(Logger) }
+
+    before do
+      described_class.instance_variable_set(:@config, nil)
+      described_class.instance_variable_set(:@logger, nil)
+    end
+
+    after do
+      described_class.instance_variable_set(:@config, nil)
+      described_class.instance_variable_set(:@logger, nil)
+    end
+
+    context 'with configuration options' do
+      let(:log_file) { double }
+      let(:log_level) { double }
+
+      before do
+        described_class.configure do |config|
+          config.log_file = log_file
+          config.log_level = log_level
+        end
+      end
+
+      it 'returns a default Logger' do
+        allow(Logger).to receive(:new).with(log_file, progname: 'RubyLLM', level: log_level).and_return(logger)
+
+        expect(described_class.logger).to eq(logger)
+      end
+    end
+
+    context 'with a custom logger' do
+      let(:log_file) { double }
+      let(:log_level) { double }
+
+      before do
+        described_class.configure do |config|
+          config.logger = logger
+          config.log_file = log_file
+          config.log_level = log_level
+        end
+      end
+
+      it 'returns a the custom Logger' do
+        expect(described_class.logger).to eq(logger)
+      end
+    end
+  end
+end
