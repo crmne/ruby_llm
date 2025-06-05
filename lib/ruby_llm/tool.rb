3bbeec43fc40a761e5d46502f9153f479a3d82e8
Eric-Guo <eric.guocz@gmail.com>
2025-06-05 17:30:47 +0800

    Need dup.

--- lib/ruby_llm/tool.rb
+++ lib/ruby_llm/tool.rb
@@ -50,7 +50,7 @@ module RubyLLM
 
     def name
       klass_name = self.class.name
-      normalized = klass_name.to_s.force_encoding('UTF-8').unicode_normalize(:nfkd)
+      normalized = klass_name.to_s.dup.force_encoding('UTF-8').unicode_normalize(:nfkd)
       normalized.encode('ASCII', replace: '')
                 .gsub(/[^a-zA-Z0-9_-]/, '-')
                 .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
