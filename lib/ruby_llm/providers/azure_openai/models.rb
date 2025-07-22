d0a24900b9f8c229155827d61f674bc3969c774b
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2025-07-22 15:31:03 -0400

    Fix models listing to restrict to some models

--- lib/ruby_llm/providers/azure_openai/models.rb
+++ lib/ruby_llm/providers/azure_openai/models.rb
@@ -7,10 +7,25 @@ module RubyLLM
       module Models
         extend OpenAI::Models
 
+        KNOWN_MODELS = [
+          'gpt-4o'
+        ].freeze
+
         module_function
 
         def models_url
           'models?api-version=2024-10-21'
+        end
+
+        def parse_list_models_response(response, slug, capabilities)
+          # select the known models only since this list from Azure OpenAI is
+          # very long
+          response.body['data'].select! do |m|
+            KNOWN_MODELS.include?(m['id'])
+          end
+          # Use the OpenAI processor for the list, keeping in mind that pricing etc
+          # won't be correct
+          super
         end
       end
     end
