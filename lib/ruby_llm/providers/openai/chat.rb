80b134b0317c53ce71c130f32d242886fd05679b
Carmine Paolino <carmine@paolino.me>
2025-08-12 13:16:59 +0200

    Fix rubocop issue

--- lib/ruby_llm/providers/openai/chat.rb
+++ lib/ruby_llm/providers/openai/chat.rb
@@ -21,9 +21,7 @@ module RubyLLM
           # Only include temperature if it's not nil (some models don't accept it)
           payload[:temperature] = temperature unless temperature.nil?
 
-          if tools.any?
-            payload[:tools] = tools.map { |_, tool| tool_for(tool) }
-          end
+          payload[:tools] = tools.map { |_, tool| tool_for(tool) } if tools.any?
 
           if schema
             # Use strict mode from schema if specified, default to true
