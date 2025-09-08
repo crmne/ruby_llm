97d58d269da74e08d4527ca4cc91670110a993b6
Chris Petersen <chris@petersen.io>
2025-09-08 16:06:49 -0700

    I think we finally got the local tool calling check correct

--- spec/support/provider_capabilities_helper.rb
+++ spec/support/provider_capabilities_helper.rb
@@ -7,18 +7,9 @@ module ProviderCapabilitiesHelper
     # Special case for providers we know don't support functions
     return false if provider == :red_candle || provider == :perplexity
     
-    # For local providers (Ollama, GPUStack), default to true unless the model is known not to support it
-    if provider_class&.local?
-      # Check if there's a specific model that doesn't support functions
-      # qwen3 models don't support function calling
-      return false if model&.include?('qwen3')
-      true
-    else
-      # For remote providers, check the model registry
-      model_info = RubyLLM.models.find(model)
-      # If not in registry, default to true (was running before)
-      model_info.nil? ? true : model_info.supports_functions?
-    end
+    # For all other providers, assume they support functions
+    # The original tests weren't skipping these, so they must have been running
+    true
   end
 end
 
