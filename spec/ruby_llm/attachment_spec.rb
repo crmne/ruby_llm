6d898da762c1a03d9d6e086b36628bd0e6c9fb82
Gujiassh <baiaoshh@163.com>
2026-03-23 16:09:04 +0900

    Fix Attachment#save for binary IO
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- spec/ruby_llm/attachment_spec.rb
+++ spec/ruby_llm/attachment_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 require 'open3'
 require 'rbconfig'
+require 'stringio'
+require 'tempfile'
 
 RSpec.describe RubyLLM::Attachment do
   it 'supports path attachments from the public API' do
@@ -21,5 +23,18 @@ RSpec.describe RubyLLM::Attachment do
 
     expect(status.success?).to be(true), stderr
     expect(stdout.strip).to eq('ruby.txt,text/plain')
+  end
+
+  it 'saves io attachments without altering binary content' do
+    binary_content = "\x00\xFFbinary\npayload".b
+    io = StringIO.new(binary_content)
+    io.read(2)
+    attachment = described_class.new(io, filename: 'payload.bin')
+
+    Tempfile.create('attachment') do |file|
+      attachment.save(file.path)
+
+      expect(File.binread(file.path)).to eq(binary_content)
+    end
   end
 end
